8984139890e9736f8a3897bc4c7f9087570435b3
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-03-11 17:29:29 +0000

    Maze side generation beginning

---
+++ Assets/MedievalRoleplay2.0/Scripts/GenerateTablePrefabsScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Runs in the editor to create many objects quickly
+// Matthew Cormack @johnjoemcbob
+// 11/03/16 17:21
+
+[ExecuteInEditMode]
+public class GenerateTablePrefabsScript : MonoBehaviour
+{
+	public Vector3 DistanceRow = new Vector3( 1, 0, 0 );
+	public Vector3 DistanceCol = new Vector3( 0, 0, 1 );
+	public Vector2 Rows = new Vector2( 0, 2 );
+	public Vector2 Columns = new Vector2( 0, 2 );
+	public GameObject Prefab;
+
+	// Flag for activation
+	public bool Generate = false;
+
+	void Update()
+	{
+		if ( Prefab && Generate )
+		{
+			for ( float row = Rows.x; row < Rows.y; row++ )
+			{
+				for ( float col = Columns.x; col < Columns.y; col++ )
+				{
+					Vector3 position = ( row * DistanceRow ) + ( col * DistanceCol );
+					Quaternion rotation = transform.rotation;
+					GameObject gobj = (GameObject) Instantiate( Prefab, position, rotation );
+					gobj.transform.SetParent( transform );
+
+					Generate = false;
+				}
+			}
+		}
+	}
+}
