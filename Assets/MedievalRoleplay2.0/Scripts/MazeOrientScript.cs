c9e742e92b5390d0acf7643d61570e57383c3be4
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-03-12 21:15:51 +0000

    Gyro Start

--- Assets/MedievalRoleplay2.0/Scripts/MazeOrientScript.cs
+++ Assets/MedievalRoleplay2.0/Scripts/MazeOrientScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 // Logic for the maze being tilted and rotated to affect the ball
@@ -12,27 +13,59 @@ public class MazeOrientScript : MonoBehaviour
 	public float ReturnMultiplier = 20;
 	public Transform CameraBuff;
 
-	private Vector2 PressedPos = Vector3.zero;
+	private Vector3 PressedPos = Vector3.zero;
 	private Vector3 TargetRotation = Vector3.zero;
+
+	// temp
+	private float ANBGLES = 0;
+
+	void Start()
+	{
+		if ( Application.platform == RuntimePlatform.Android )
+		{
+			Input.gyro.enabled = true;
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		}
+	}
 
 	void Update()
 	{
-		if ( Input.GetMouseButtonDown( 0 ) )
+		if ( ( PressedPos.x == 0 ) && ( PressedPos.y == 0 ) )
 		{
-			PressedPos.x = Input.mousePosition.x;
-			PressedPos.y = Input.mousePosition.y;
+			PressedPos.x = Input.gyro.gravity.x;
+			PressedPos.y = Input.gyro.gravity.y;
+			PressedPos.z = Input.gyro.gravity.y;
 		}
-		else if ( Input.GetMouseButton( 0 ) )
+		if ( Application.platform == RuntimePlatform.Android )
 		{
-			// Affect differently depending on camera rotation
-			// Mouse x and y to rotation x and z (or something)
+			TargetRotation.z = ( PressedPos.x - Input.gyro.gravity.x ) * MoveMultiplier * MaxRotation;
+			TargetRotation.x = ( PressedPos.y - Input.gyro.gravity.y ) * MoveMultiplier * MaxRotation;
+			TargetRotation.x = ( PressedPos.z - Input.gyro.gravity.z ) * MoveMultiplier * MaxRotation;
 			GetTargetFromCamera();
+			GetComponentInChildren<Text>().text = string.Format( "{0}: {1} {2} {3}", ANBGLES, Input.gyro.gravity.x, Input.gyro.gravity.y, Input.gyro.gravity.z );
 		}
 		else
 		{
-			// Lerp the target slowly back when not holding mouse down
-			TargetRotation += ( Vector3.zero - TargetRotation ) * Time.deltaTime * ReturnMultiplier;
-        }
+			if ( Input.GetMouseButtonDown( 0 ) )
+			{
+				PressedPos.x = Input.mousePosition.x;
+				PressedPos.y = Input.mousePosition.y;
+			}
+			else if ( Input.GetMouseButton( 0 ) )
+			{
+				// Affect differently depending on camera rotation
+				// Mouse x and y to rotation x and z (or something)
+				TargetRotation.z = ( ( PressedPos.x - Input.mousePosition.x ) / Screen.width * MoveMultiplier ) * MaxRotation;
+				TargetRotation.x = -( ( PressedPos.y - Input.mousePosition.y ) / Screen.height * MoveMultiplier ) * MaxRotation;
+
+				GetTargetFromCamera();
+			}
+			else
+			{
+				// Lerp the target slowly back when not holding mouse down
+				TargetRotation += ( Vector3.zero - TargetRotation ) * Time.deltaTime * ReturnMultiplier;
+			}
+		}
 
 		// Lerp the rotation of the maze
 		Quaternion target = Quaternion.Euler( TargetRotation );
@@ -47,9 +80,6 @@ public class MazeOrientScript : MonoBehaviour
 
 	void GetTargetFromCamera()
 	{
-		TargetRotation.z = ( ( PressedPos.x - Input.mousePosition.x ) / Screen.width * MoveMultiplier ) * MaxRotation;
-		TargetRotation.x = -( ( PressedPos.y - Input.mousePosition.y ) / Screen.height * MoveMultiplier ) * MaxRotation;
-
 		int angle = 0;
 		{
 			float yaw = CameraBuff.localEulerAngles.y;
@@ -65,22 +95,48 @@ public class MazeOrientScript : MonoBehaviour
 				}
 			}
 		}
-		switch ( angle )
+		ANBGLES = angle;
+		if ( Application.platform == RuntimePlatform.Android )
 		{
-			case 0:
-				break;
-			case 1:
-				break;
-			case 2:
-				TargetRotation.x *= -1;
-				TargetRotation.z *= -1;
-				break;
-			case 3:
-				TargetRotation.x *= -1;
-				TargetRotation.z *= -1;
-				break;
-			default:
-				break;
+			Vector3 temp = TargetRotation;
+			switch ( angle )
+			{
+				case 0:
+					
+					break;
+				case 1:
+					break;
+				case 2:
+					TargetRotation.x *= -1;
+					TargetRotation.z *= -1;
+					break;
+				case 3:
+					TargetRotation.x *= -1;
+					TargetRotation.z *= -1;
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			switch ( angle )
+			{
+				case 0:
+					break;
+				case 1:
+					break;
+				case 2:
+					TargetRotation.x *= -1;
+					TargetRotation.z *= -1;
+					break;
+				case 3:
+					TargetRotation.x *= -1;
+					TargetRotation.z *= -1;
+					break;
+				default:
+					break;
+			}
 		}
 	}
 
